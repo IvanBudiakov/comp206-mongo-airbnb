a2869bb6c98d57609f2d0895185cd4d7006657af
shivoqu <ivan.budiakov@student.sl.on.ca>
2021-11-12 13:23:16 -0500

    delete commented code

--- mongo/queries.js
+++ mongo/queries.js
@@ -1,43 +1,5 @@
 const mongoClient = require("./config");
 
-// async function findListing1(response)
-// {
-//   console.log('Find Listing');
-//   let connection = await mongoClient.connect()
-//   console.log('Connected');
-//   let db = await connection.db('sample_airbnb');
-//   console.log('open airbnb databse')
-
-//   let listingsAndReviews = await db.collection('listingsAndReviews')
-//   console.log('select listings and reviews')
-
-//   let listing = await listingsAndReviews.findOne({})
-
-//   console.log('Close db')
-
-//   response.send(listing)
-//   connection.close()  
-// }
-
-// async function findListing2 (response)
-// {
-//   try{
-//     var connection = await mongoClient.connect()
-//     let db = await connection.db('sample_airbnb');
-//     let listingsAndReviews = await db.collection('listingsAndReviews')
-//     let listing = await listingsAndReviews.findOne({})
-//     response.send(listing)
-//   }
-//   catch(error)
-//   {
-//     console.log(error)
-//     response.send(error)
-//   }
-//   finally{
-//     // This is where any cleanup code goes
-//     connection.close()
-//   }
-// }
 
 async function findListing(response, criteria)
 {
@@ -78,4 +40,7 @@ async function findListings (response, criteria)
 // let findListing = findListing3
 
 module.exports = {findListing, findListings}  
-// Shortcut for {findListing:findListing; findListings:findListings}
+// Shortcut for {
+//   findListing:findListing; 
+//   findListings:findListings
+// }
